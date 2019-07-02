6bf5014ad911c6dae885ea219f943de819353abb
littlelailo <littlelailo@yahoo.com>
2019-07-02 22:50:56 +0200

    syms => img

--- src/patchfinder.c
+++ src/patchfinder.c
@@ -1,24 +1,24 @@
 #include "patchfinder.h"
 #include "a64.h"
 
-uint64_t libjake_find_str(jake_img_t syms,char * str) {
-	uint64_t found = (uint64_t)memmem(syms->map,syms->mapsize,str,strlen(str));
+uint64_t libjake_find_str(jake_img_t img,char * str) {
+	uint64_t found = (uint64_t)memmem(img->map,img->mapsize,str,strlen(str));
 	if (found == 0) {
 		P_LOG_DBG("Unable to find the string %s\n",str);
 		FAILED_TO_FIND();	
 		return 0;
 	}
-	return found-((uint64_t)syms->map)+LIBJAKE_KERNEL_BASE;
+	return found-((uint64_t)img->map)+LIBJAKE_KERNEL_BASE;
 }
-uint64_t find_xref(jake_img_t syms,uint64_t start_addr,uint64_t xref_address) {
+uint64_t find_xref(jake_img_t img,uint64_t start_addr,uint64_t xref_address) {
 	// we search for either adr or adrp + (add/ldr)
 	// this also verifies that the input register of ldr/add is the right register
 	xref_address -= LIBJAKE_KERNEL_BASE; // we search on some mapping so we will just use the offset inside of that mapping as the address (when we return we slide it back)
 	uint64_t search_start_offset = start_addr - LIBJAKE_KERNEL_BASE; // same here
 	uint64_t adr_value = 0x0;
 	char current_reg = -1;
-	for (uint64_t addr = search_start_offset & 3; addr < syms->mapsize; addr += 4) {
-		uint32_t ins = *(uint32_t *)(syms->map + addr);
+	for (uint64_t addr = search_start_offset & 3; addr < img->mapsize; addr += 4) {
+		uint32_t ins = *(uint32_t *)(img->map + addr);
 		if (is_adr((adr_t*)&ins)) {
 			adr_value = addr + get_adr_off((adr_t*)&ins);
 			current_reg = ((adr_t*)&ins)->Rd;
@@ -58,7 +58,7 @@ uint64_t find_xref(jake_img_t syms,uint64_t start_addr,uint64_t xref_address) {
 	FAILED_TO_FIND();
 	return 0;
 }
-uint64_t find_load_skip(jake_img_t syms, uint64_t start_addr, uint64_t n_instr, bool backwards, int toskip) {
+uint64_t find_load_skip(jake_img_t img, uint64_t start_addr, uint64_t n_instr, bool backwards, int toskip) {
 	// we search for either adr or adrp + (add/ldr)
 	// this also verifies that the input register of ldr/add is the right register
 	uint64_t adr_value = 0x0;
@@ -68,7 +68,7 @@ uint64_t find_load_skip(jake_img_t syms, uint64_t start_addr, uint64_t n_instr, 
 	int step = 4;
 	if (backwards) {step = -step;}
 	for (uint64_t addr = start_off; addr < (start_off + n_instr*4); addr += step) {
-		uint32_t ins = *(uint32_t *)(syms->map + addr);
+		uint32_t ins = *(uint32_t *)(img->map + addr);
 		if (is_adr((adr_t*)&ins)) {
 			if (toskip > 0) {toskip--;continue;}
 			adr_value = addr + get_adr_off((adr_t*)&ins);
@@ -114,14 +114,14 @@ uint64_t find_load_skip(jake_img_t syms, uint64_t start_addr, uint64_t n_instr, 
 	FAILED_TO_FIND();
 	return 0;
 }
-#define find_load(syms,start,n_instr,backwards) find_load_skip(syms,start,n_instr,backwards,0)
+#define find_load(img,start,n_instr,backwards) find_load_skip(img,start,n_instr,backwards,0)
 
-uint64_t find_str_xref(jake_img_t syms, char * str) {
-	return find_xref(syms,LIBJAKE_KERNEL_BASE,libjake_find_str(syms,str));
+uint64_t find_str_xref(jake_img_t img, char * str) {
+	return find_xref(img,LIBJAKE_KERNEL_BASE,libjake_find_str(img,str));
 }
 
-uint64_t find_swapprefix(jake_img_t syms) {
-	uint64_t found = libjake_find_str(syms,"/private/var/vm/swapfile");
+uint64_t find_swapprefix(jake_img_t img) {
+	uint64_t found = libjake_find_str(img,"/private/var/vm/swapfile");
 	if (found == 0) {
 		P_LOG_DBG("Unable to find swapfile string\n");
 		FAILED_TO_FIND();
@@ -130,29 +130,29 @@ uint64_t find_swapprefix(jake_img_t syms) {
 	return found;
 }
 
-uint64_t find_trustcache(jake_img_t syms) {
-	uint64_t str_ref = find_str_xref(syms,"%s: unable to obtain AKS services for checking unlock state,");
+uint64_t find_trustcache(jake_img_t img) {
+	uint64_t str_ref = find_str_xref(img,"%s: unable to obtain AKS services for checking unlock state,");
 	if (!str_ref) {
 		P_LOG_DBG("Unable to find string for trustcache\n");
 		FAILED_TO_FIND();
 		return 0x0;
 	}
 	P_LOG_DBG("Found string ref for trustcache @ %llx\n",str_ref);
-	uint64_t found = find_load_skip(syms,first_load,15,false,1);
+	uint64_t found = find_load_skip(img,str_ref,15,false,1);
 	P_LOG_DBG("Found trustcache @ %llx\n",found);
 	return found;
 }
 
-uint64_t find_realhost(jake_img_t syms) {
+uint64_t find_realhost(jake_img_t img) {
 	// for realhost it's pretty simple because the function host_priv_self returns it and it has a symbol
-	uint64_t host_priv_self = jake_find_symbol(syms,"_host_priv_self");
+	uint64_t host_priv_self = jake_find_symbol(img,"_host_priv_self");
 	if (!host_priv_self) {
 		P_LOG_DBG("Unable to find host_priv_self symbol\n");
 		FAILED_TO_FIND();
 		return 0x0;
 	}
 	/*
-	uint32_t * ins_ptr = (uint32_t *)(syms->map + (host_priv_self - LIBJAKE_KERNEL_BASE));
+	uint32_t * ins_ptr = (uint32_t *)(img->map + (host_priv_self - LIBJAKE_KERNEL_BASE));
 	if (!is_adrp(ins_ptr)) {
 		P_LOG_DBG("Expected an adrp as the first instruction\n");
 		return 0x0;
@@ -167,21 +167,21 @@ uint64_t find_realhost(jake_img_t syms) {
 	realhost += get_add_sub_imm((add_imm_t *)ins_ptr);
 	P_LOG_DBG("Realhost is at %llx\n",realhost);
 	*/
-	uint64_t found = find_load(syms,host_priv_self,2,false); // we only give it two instructions because those have to be the first two in this function;
+	uint64_t found = find_load(img,host_priv_self,2,false); // we only give it two instructions because those have to be the first two in this function;
 	P_LOG_DBG("Found realhost @ %llx\n",found);
 	return found;
 }
 
-uint64_t find_zonemap(jake_img_t syms) {
+uint64_t find_zonemap(jake_img_t img) {
 	// for the zone map we need to search for the string zone_init: kmem_suballoc failed and then use the first adrp+add above
-	uint64_t str_ref = find_str_xref(syms,"\"zone_init: kmem_suballoc failed");
+	uint64_t str_ref = find_str_xref(img,"\"zone_init: kmem_suballoc failed");
 	if (!str_ref) {
 		P_LOG_DBG("Unable to find the string for zonemap\n");
 		FAILED_TO_FIND();
 		return 0x0;
 	}
 	P_LOG_DBG("Found string for zonemap @ %llx\n",str_ref);
-	uint64_t found = find_load(syms,str_ref-2*4/*this has to jump over it's own adrp, add lol*/, 30, true); // move at max 30 instructions upwards and try to find the adrp+add
+	uint64_t found = find_load(img,str_ref-2*4/*this has to jump over it's own adrp, add lol*/, 30, true); // move at max 30 instructions upwards and try to find the adrp+add
 	P_LOG_DBG("Found zonemap @ %llx\n",found);
 	return found;
 }
