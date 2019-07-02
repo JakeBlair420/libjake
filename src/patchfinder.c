4f0d360024ff9746bc52ec786ef73c51cf3a21c1
littlelailo <littlelailo@yahoo.com>
2019-07-02 21:05:51 +0200

    [Bugfixes] only trustcache left

--- src/patchfinder.c
+++ src/patchfinder.c
@@ -1,6 +1,15 @@
 #include "patchfinder.h"
 #include "a64.h"
 
+uint64_t libjake_find_str(jake_symbols_t syms,char * str) {
+	uint64_t found = memmem(syms->map,syms->mapsize,str,strlen(str));
+	if (found == 0) {
+		P_LOG_DBG("Unable to find the string %s\n",str);
+		FAILED_TO_FIND();	
+		return 0;
+	}
+	return found-((uint64_t)syms->map)+LIBJAKE_KERNEL_BASE;
+}
 uint64_t find_xref(jake_symbols_t syms,uint64_t start_addr,uint64_t xref_address) {
 	// we search for either adr or adrp + (add/ldr)
 	// this also verifies that the input register of ldr/add is the right register
@@ -45,6 +54,8 @@ uint64_t find_xref(jake_symbols_t syms,uint64_t start_addr,uint64_t xref_address
 			return addr + LIBJAKE_KERNEL_BASE;
 		}
 	}
+	P_LOG_DBG("Unable to find xref for %llx starting from address %llx\n",xref_address+LIBJAKE_KERNEL_BASE,start_addr);
+	FAILED_TO_FIND();
 	return 0;
 }
 
@@ -98,43 +109,37 @@ uint64_t find_load(jake_symbols_t syms, uint64_t start_addr, uint64_t n_instr, b
 			adr_value = 0x0;
 		}
 	}
+	P_LOG_DBG("Unable to find load @ %llx walking %x (1 == backwards) for 0x%llx instructions\n",start_addr,backwards,n_instr);
+	FAILED_TO_FIND();
 	return 0;
 }
 
 uint64_t find_str_xref(jake_symbols_t syms, char * str) {
-	return find_xref(syms,0,libjake_find_str(syms,str));
+	return find_xref(syms,LIBJAKE_KERNEL_BASE,libjake_find_str(syms,str));
 }
 
 uint64_t find_swapprefix(jake_symbols_t syms) {
-	return find_str_xref(syms,"/private/var/vm/swapfile");
+	uint64_t found = libjake_find_str(syms,"/private/var/vm/swapfile");
+	if (found == 0) {
+		P_LOG_DBG("Unable to find swapfile string\n");
+		FAILED_TO_FIND();
+	}
+	P_LOG_DBG("Found swapprefix str @ %llx\n",found);
+	return found;
 }
 
 uint64_t find_trustcache(jake_symbols_t syms) {
-	uint64_t str_ref = find_str_xref(syms,"com.apple.MobileFileIntegrity");
+	uint64_t str_ref = find_str_xref(syms,"%s: unable to obtain AKS services for checking unlock state,");
 	if (!str_ref) {
 		P_LOG_DBG("Unable to find string for trustcache\n");
+		FAILED_TO_FIND();
 		return 0x0;
 	}
-	P_LOG_DBG("Found string for trustcache @ %llx\n",str_ref);
-	uint64_t start = str_ref-LIBJAKE_KERNEL_BASE;
-	bool found_first_bl = false;
-	uint64_t target_func = 0x0;
-	for (uint64_t addr = start; addr < (start + 100*4); addr+=4) {
-		uint32_t ins = *(uint32_t *)(syms->map + addr);
-		if (is_bl((bl_t*)&ins)) {
-			if (found_first_bl) {
-				target_func = get_bl_off((bl_t*)&ins) + addr;
-				break;
-			}
-			found_first_bl = true;
-		}
-	}
-	if (target_func == 0x0) {
-		P_LOG_DBG("Unable to find the target function for trustcache\n");
-		return 0x0;
-	}
-	P_LOG_DBG("Trustcache function @ %llx\n",target_func + LIBJAKE_KERNEL_BASE);
-	return find_load(syms,target_func + LIBJAKE_KERNEL_BASE, 15, false);	
+	P_LOG_DBG("Found string ref for trustcache @ %llx\n",str_ref);
+	uint64_t first_load = find_load(syms,str_ref,15,false);
+	uint64_t found = find_load(syms,first_load+4,15,false);
+	P_LOG_DBG("Found trustcache @ %llx\n",found);
+	return found;
 }
 
 uint64_t find_realhost(jake_symbols_t syms) {
@@ -142,6 +147,7 @@ uint64_t find_realhost(jake_symbols_t syms) {
 	uint64_t host_priv_self = find_symbol(syms,"_host_priv_self");
 	if (!host_priv_self) {
 		P_LOG_DBG("Unable to find host_priv_self symbol\n");
+		FAILED_TO_FIND();
 		return 0x0;
 	}
 	/*
@@ -160,16 +166,21 @@ uint64_t find_realhost(jake_symbols_t syms) {
 	realhost += get_add_sub_imm((add_imm_t *)ins_ptr);
 	P_LOG_DBG("Realhost is at %llx\n",realhost);
 	*/
-	return find_load(syms,host_priv_self,2,false); // we only give it two instructions because those have to be the first two in this function;
+	uint64_t found = find_load(syms,host_priv_self,2,false); // we only give it two instructions because those have to be the first two in this function;
+	P_LOG_DBG("Found realhost @ %llx\n",found);
+	return found;
 }
 
 uint64_t find_zonemap(jake_symbols_t syms) {
 	// for the zone map we need to search for the string zone_init: kmem_suballoc failed and then use the first adrp+add above
-	uint64_t str_ref = find_str_xref(syms,"zone_init: kmem_suballoc failed");
+	uint64_t str_ref = find_str_xref(syms,"\"zone_init: kmem_suballoc failed");
 	if (!str_ref) {
 		P_LOG_DBG("Unable to find the string for zonemap\n");
+		FAILED_TO_FIND();
 		return 0x0;
 	}
 	P_LOG_DBG("Found string for zonemap @ %llx\n",str_ref);
-	return find_load(syms,str_ref, 15, true); // move at max 15 instructions upwards and try to find the adrp+add
+	uint64_t found = find_load(syms,str_ref-2*4/*this has to jump over it's own adrp, add lol*/, 30, true); // move at max 30 instructions upwards and try to find the adrp+add
+	P_LOG_DBG("Found zonemap @ %llx\n",found);
+	return found;
 }
--- src/patchfinder.h
+++ src/patchfinder.h
@@ -1,11 +1,13 @@
- #include <string.h> // memmem
+#include <string.h> // memmem
+#include <stdlib.h> // exit
 
 #include "common.h"
 #include "symbols.h"
 
 #ifndef LIBJAKE_PATCHFINDER
 #define LIBJAKE_PATCHFINDER
-#define libjake_find_str(syms,str) ((uint64_t)memmem(syms->map,syms->mapsize,str,sizeof(str))-((uint64_t)syms->map)+LIBJAKE_KERNEL_BASE)
+#define FAILED_TO_FIND() exit(1)
+uint64_t libjake_find_str(jake_symbols_t syms, char * str);
 uint64_t find_xref(jake_symbols_t syms,uint64_t start_addr,uint64_t xref_address);
 uint64_t find_trustcache(jake_symbols_t syms);
 uint64_t find_swapprefix(jake_symbols_t syms);
